888ed81eaf30c4e8ce55769665250cdef270c3f4
ningunickdisponible <58410052+ningunickdisponible@users.noreply.github.com>
2019-12-03 18:42:14 -0300

    Primera subida

---
+++ pruebasssss/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace pruebasssss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la calculadora!");
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Ingrese un numero");
+            int var1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            int var2 = int.Parse(Console.ReadLine());
+            bool entra = true;
+            while (entra)
+            {
+                entra = false;
+                Console.WriteLine("Ingrese que operacion quiere hacer:");
+                Console.WriteLine("\t S-Suma");
+                Console.WriteLine("\t R-Resta");
+                Console.WriteLine("\t M-Multiplicacion");
+                Console.WriteLine("\t D-Division");
+
+                string operacion = Console.ReadLine();
+                switch (operacion.ToUpper())
+                {
+                    case "S":
+                        sumar(var1, var2);
+                        break;
+                    case "R":
+                        Console.WriteLine(var1 - var2);
+                        break;
+                    case "M":
+                        Console.WriteLine(var1 * var2);
+                        break;
+                    case "D":
+                        Console.WriteLine(var1 / var2);
+                        break;
+                    default:
+                        //Aca podes poner cualquier cosa, total la condicion del while, solamente va a salir si es S, R, M o D.
+                        Console.WriteLine("La opcion elegida no es valida, reingrese otra opcion");
+                        entra = true;
+                        break;
+                }
+            }
+        }
+
+        static void sumar(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+
+    }
+}
